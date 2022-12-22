73fead3aa050104560d087187aad1fa77d8f7e47
mor3hate <thelokkedmail3@gmail.com>
2022-12-23 02:52:21 +0300

    Fix(data)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import { ITechnologies } from '@/shared/types/technologies.types'
 import { IHome } from '@/screens/home/home.interface'
 import { IProjects } from '@/shared/types/projects.types'
 import { IAbout } from '@/shared/types/about-me.types'
+import { ITechnologyGalleryItem } from '@/ui/technologies-gallery/technologies.interface'
+import { imageBuilder } from '@/helpers/image.helper'
+import { IProjectContainerItem } from '@/ui/projects-container/projects-interface'
+import { IAboutContainerItem } from '@/ui/about-container/about-container.interface'
 
 export default function HomePage({ technologies, projects, about }: IHome) {
 	return <Home technologies={technologies} projects={projects} about={about} />
@@ -14,21 +18,35 @@ export async function getStaticProps() {
 		const { technologies: technologiesData } = await fetchHelper<ITechnologies>(
 			'getTechnologies'
 		)
-		//@ts-ignore
-		const technologies = []
+		const technologies: ITechnologyGalleryItem[] = technologiesData.map(t => ({
+			imagePath: imageBuilder(t.image.asset._ref),
+			name: t.name || '',
+			progress: t.progress,
+			_id: t._id
+		}))
 
 		const { projects: projectsData } = await fetchHelper<IProjects>(
 			'getProjects'
 		)
-		//@ts-ignore
-		const projects = []
+
+		const projects: IProjectContainerItem[] = projectsData.map(p => ({
+			imagePath: imageBuilder(p.image.asset._ref),
+			_id: p._id,
+			technologies: p.technologies,
+			title: p.title,
+			links: p.links
+		}))
 
 		const { about: aboutData } = await fetchHelper<IAbout>('getAbout')
-		//@ts-ignore
-		const about = []
+
+		const about: IAboutContainerItem[] = [
+			{
+				bio: aboutData[0].bio,
+				imagePath: imageBuilder(aboutData[0].image.asset._ref)
+			}
+		]
 
 		return {
-			//@ts-ignore
 			props: { technologies, projects, about },
 			revalidate: 100
 		}
