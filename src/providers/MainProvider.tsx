14af2624d8b761a3725515d83ebea8bbae34994d
mor3hate <thelokkedmail3@gmail.com>
2023-01-13 00:51:45 +0300

    Fix(styles)

--- src/providers/MainProvider.tsx
+++ src/providers/MainProvider.tsx
@@ -1,21 +1,13 @@
 import Layout from '@/components/layout/Layout'
-import { Roboto } from '@next/font/google'
 import { PropsWithChildren } from 'react'
 
 import 'react-toastify/dist/ReactToastify.min.css'
 import { ToastContainer } from 'react-toastify'
 
-const roboto = Roboto({
-	weight: ['400', '500', '700'],
-	style: ['normal'],
-	subsets: ['latin'],
-	preload: true
-})
-
 export default function MainProvider({ children }: PropsWithChildren) {
 	return (
 		<Layout>
-			<div className={roboto.className}>{children}</div>
+			{children}
 			<ToastContainer />
 		</Layout>
 	)
